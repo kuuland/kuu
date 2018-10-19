aeb6ac4aba0eef4e8a73c231d48c4a5af84c602f
yinfxs <yinfxs@gmail.com>
2018-10-19 14:53:56 +0800

    update std

--- kuu_test.go
+++ kuu_test.go
@@ -1,7 +1,9 @@
 package kuu
 
 import (
+	"encoding/json"
 	"fmt"
+	"testing"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,4 +19,60 @@ func ExampleNew() {
 	k.Run(":8080")
 	// Output:
 	// Hello kuu.
+}
+
+func TestStd(t *testing.T) {
+	data, err := json.Marshal(Std(1001, "sign up success", 0))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+}
+func ExampleStd() {
+	data, err := json.Marshal(Std(1001, "sign up success", 0))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+	// Output:
+	// {"code":0,"data":1001,"msg":"sign up success"}
+}
+
+func TestStdOK(t *testing.T) {
+	data, err := json.Marshal(StdOK("kuu"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+}
+func ExampleStdOK() {
+	data, err := json.Marshal(StdOK("kuu"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+	// Output:
+	// {"code":0,"data":"kuu"}
+}
+func TestStdError(t *testing.T) {
+	data, err := json.Marshal(StdError("Login failed"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+}
+func ExampleStdError() {
+	data, err := json.Marshal(StdError("Login failed"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+	// Output:
+	// {"code":-1,"msg":"Login failed"}
 }
