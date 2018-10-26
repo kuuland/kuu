fbe1aa82a11dd875ea5bb8d174c7e618040fb528
yinfxs <yinfxs@gmail.com>
2018-10-26 21:57:40 +0800

    add base models

--- schema.go
+++ schema.go
@@ -1,9 +1,5 @@
 package kuu
 
-import (
-	"time"
-)
-
 // IConfig 模型配置接口，数据模型通过实现该接口来增强模型配置
 type IConfig interface {
 	Config() H
@@ -27,15 +23,6 @@ type SchemaField struct {
 	Type     string
 	Required bool
 	Default  interface{}
-}
-
-// Model 定义了数据模型的一些基本字段
-type Model struct {
-	CreatedBy interface{} `name:"创建人"`
-	CreatedAt time.Time   `name:"创建时间"`
-	UpdatedBy interface{} `name:"修改人"`
-	UpdatedAt time.Time   `name:"修改时间"`
-	IsDeleted bool        `name:"是否已删除"`
 }
 
 // IModel 定义了模型持久化操作接口
