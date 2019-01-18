8cbfdc2a5a75efe96ce14e19df264762a65edc01
yinfxs <yinfxs@gmail.com>
2019-01-18 09:44:32 +0800

    fix cond["_id"]

--- mods/mongo/model.go
+++ mods/mongo/model.go
@@ -79,7 +79,6 @@ func (m *Model) Create(data interface{}) ([]interface{}, error) {
 				doc["CreatedBy"] = doc["CreatedBy"].(bson.ObjectId)
 			}
 		}
-		//transformJoinDataWhenSave(doc, m.schema)
 		docs[index] = doc
 	}
 	C := C(m.Collection)
@@ -341,6 +340,11 @@ func (m *Model) ID(id interface{}, data interface{}) error {
 	}
 	if p.Cond == nil {
 		p.Cond = make(kuu.H)
+	}
+	if p.Cond["_id"] != nil {
+		if v, ok := p.Cond["_id"].(string); ok {
+			p.Cond["_id"] = bson.ObjectIdHex(v)
+		}
 	}
 	if p.ID == "" {
 		kuu.JSONConvert(id, p)
@@ -757,68 +761,4 @@ func getHexMap(ret []kuu.H) kuu.H {
 		}
 	}
 	return hexMap
-}
-
-func transformJoinDataWhenSave(doc kuu.H, schema *kuu.Schema) {
-	fields := getJoinFields(schema, nil)
-	for _, field := range fields {
-		rawData := doc[field.Code]
-		if rawData == nil {
-			continue
-		}
-		if field.IsArray {
-			rawD := rawData.([]interface{})
-			newD := []bson.ObjectId{}
-			for _, t := range rawD {
-				var item kuu.H
-				kuu.JSONConvert(t, item)
-				if item == nil {
-					continue
-				}
-				if item["_id"] != "" {
-					var id bson.ObjectId
-					switch item["_id"].(type) {
-					case string:
-						id = bson.ObjectIdHex(item["_id"].(string))
-
-					case bson.ObjectId:
-						id = item["_id"].(bson.ObjectId)
-					}
-					if id != "" {
-						newD = append(newD, id)
-					}
-				}
-			}
-			doc[field.Code] = newD
-		} else {
-			var item kuu.H
-			kuu.JSONConvert(rawData, item)
-			if item == nil {
-				continue
-			}
-			var id bson.ObjectId
-			if item["_id"] != "" {
-				switch item["_id"].(type) {
-				case string:
-					id = bson.ObjectIdHex(item["_id"].(string))
-
-				case bson.ObjectId:
-					id = item["_id"].(bson.ObjectId)
-				}
-			}
-			doc[field.Code] = id
-		}
-	}
-}
-
-// TODO transformJoinDataOnSave
-// object => _id or []object => []_id
-func transformJoinDataOnSave() {
-
-}
-
-// TODO transformJoinDataOnQuery
-// _id => object._id or []_id  => []object._id
-func transformJoinDataOnQuery() {
-
 }
