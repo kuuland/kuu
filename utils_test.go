2585b65eddfce1f04c52797d622ea46f2bb621fa
yinfxs <yinfxs@gmail.com>
2019-12-11 17:01:43 +0800

    fix: 增加TestSetUrlQuery

--- utils_test.go
+++ utils_test.go
@@ -11,3 +11,25 @@ func TestRandCode(t *testing.T) {
 	t.Log(RandCode())
 	t.Log(RandCode(10))
 }
+
+func TestSetUrlQuery(t *testing.T) {
+	var u string
+
+	u = "/mp"
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}))
+
+	u = "/mp?a=1&b=2"
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}))
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}, true))
+
+	u = "https://www.example.com/mp"
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}))
+
+	u = "https://www.example.com/mp?a=1&b=2"
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}))
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}, true))
+
+	u = "https://www.example.com/admin/?a=1&b=2#/test?h=1&j=ok"
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}))
+	t.Log(SetUrlQuery(u, map[string]interface{}{"t": 1, "s": "hello"}, true))
+}
