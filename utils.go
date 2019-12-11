55acaa572d68c6609b22e03ed6ef3de36d9a419b
yinfxs <yinfxs@gmail.com>
2019-12-11 16:58:48 +0800

    feat: 增加工具函数SetUrlQuery

--- utils.go
+++ utils.go
@@ -304,3 +304,17 @@ func DecodeURIComponent(str string) string {
 	}
 	return r
 }
+
+// SetUrlQuery
+func SetUrlQuery(rawUrl string, values map[string]interface{}, replace ...bool) string {
+	u, _ := url.Parse(rawUrl)
+	if len(replace) > 0 && replace[0] {
+		u.RawQuery = ""
+	}
+	query := u.Query()
+	for k, v := range values {
+		query.Set(k, fmt.Sprintf("%v", v))
+	}
+	u.RawQuery = query.Encode()
+	return u.String()
+}
