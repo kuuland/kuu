0b93fe3d93e2889859dd34859d7021adfc2e610d
yinfxs <yinfxs@gmail.com>
2020-05-21 16:27:47 +0800

    feat(job): 调整AddJob接口，增加任务名称，自行输出任务执行日志、异常处理等；默认通过缓存保证每次调度线性执行

--- job.go
+++ job.go
@@ -1,6 +1,11 @@
 package kuu
 
-import "github.com/robfig/cron/v3"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/robfig/cron/v3"
+	"os"
+)
 
 // DefaultCron (set option 5 cron to convet 6 cron)
 var DefaultCron = cron.New(cron.WithSeconds())
@@ -12,6 +17,22 @@ type Job struct {
 }
 
 // AddJob
-func AddJob(spec string, cmd func()) (cron.EntryID, error) {
-	return DefaultCron.AddFunc(spec, cmd)
+func AddJob(spec string, name string, cmd func() error) (cron.EntryID, error) {
+	return DefaultCron.AddFunc(spec, func() {
+		if v := os.Getenv("JOB_PARALLEL"); v == "" {
+			// 基于缓存进行拦截，避免多实例重复执行
+			k := fmt.Sprintf("job_%s_running", name)
+			if GetCacheString(k) != "" {
+				return
+			}
+			SetCacheString(k, "1")
+			defer DelCache(k)
+		}
+
+		INFO("----------- Job '%s' start -----------", name)
+		if err := cmd(); err != nil {
+			ERROR(errors.Wrap(err, fmt.Sprintf("Job '%s'", name)))
+		}
+		INFO("----------- Job '%s' finish -----------", name)
+	})
 }
