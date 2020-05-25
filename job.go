de7c98ef00179938e0580a76ffc6c4f5ce42ee6b
yinfxs <yinfxs@gmail.com>
2020-05-25 14:51:05 +0800

    fix(job): 增加KUU_JOB处理简单定时任务场景

--- job.go
+++ job.go
@@ -2,6 +2,7 @@ package kuu
 
 import (
 	"fmt"
+	"github.com/asaskevich/govalidator"
 	"github.com/pkg/errors"
 	"github.com/robfig/cron/v3"
 	"os"
@@ -10,6 +11,20 @@ import (
 
 // DefaultCron (set option 5 cron to convet 6 cron)
 var DefaultCron = cron.New(cron.WithSeconds())
+
+var (
+	jobCmds   = make(map[cron.EntryID]bool)
+	jobCmdsMu sync.RWMutex
+)
+
+// Job
+type Job struct {
+	Spec        string              `json:"spec" valid:"required"`
+	Cmd         func(c *JobContext) `json:"-,omitempty" valid:"required"`
+	Name        string              `json:"name" valid:"required"`
+	RunAfterAdd bool                `json:"runAfterAdd"`
+	EntryID     cron.EntryID        `json:"entryID,omitempty"`
+}
 
 // JobContext
 type JobContext struct {
@@ -25,32 +40,57 @@ func (c *JobContext) Error(err error) {
 	c.errs = append(c.errs, err)
 }
 
-// AddJob
-func AddJob(spec string, name string, cmd func(c *JobContext)) (cron.EntryID, error) {
-	return DefaultCron.AddFunc(spec, func() {
-		if v := os.Getenv("JOB_PARALLEL"); v == "" {
-			// 基于缓存进行拦截，避免多实例重复执行
-			k := fmt.Sprintf("job_%s_running", name)
-			if GetCacheString(k) != "" {
-				return
-			}
-			SetCacheString(k, "1")
-			defer DelCache(k)
-		}
+// AddJobEntry
+func AddJobEntry(j *Job) error {
+	if os.Getenv("KUU_JOB") == "" {
+		return nil
+	}
 
-		INFO("----------- Job '%s' start -----------", name)
+	if _, err := govalidator.ValidateStruct(j); err != nil {
+		return err
+	}
+
+	cmd := func() {
+		jobCmdsMu.Lock()
+		defer jobCmdsMu.Unlock()
+
+		if jobCmds[j.EntryID] {
+			return
+		}
+		jobCmds[j.EntryID] = true
+		INFO("----------- Job '%s' start -----------", j.Name)
 
 		c := &JobContext{
-			name: name,
+			name: j.Name,
 			l:    new(sync.RWMutex),
 		}
-		cmd(c)
+		j.Cmd(c)
 		if len(c.errs) > 0 {
 			for i, err := range c.errs {
-				c.errs[i] = errors.Wrap(err, fmt.Sprintf("Job '%s' execute error", name))
+				c.errs[i] = errors.Wrap(err, fmt.Sprintf("Job '%s' execute error", j.Name))
 			}
 			ERROR(c.errs)
 		}
-		INFO("----------- Job '%s' finish -----------", name)
-	})
+		INFO("----------- Job '%s' finish -----------", j.Name)
+		jobCmds[j.EntryID] = false
+	}
+	if j.RunAfterAdd {
+		cmd()
+	}
+	v, err := DefaultCron.AddFunc(j.Spec, cmd)
+	if err == nil {
+		j.EntryID = v
+	}
+	return err
+}
+
+// AddJob
+func AddJob(spec string, name string, cmd func(c *JobContext)) (cron.EntryID, error) {
+	job := Job{
+		Spec: spec,
+		Name: name,
+		Cmd:  cmd,
+	}
+	err := AddJobEntry(&job)
+	return job.EntryID, err
 }
