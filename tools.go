edf313be41a18e34f8a0a965c4b80c2ef5eec7ce
danielyin <yinfxs@gmail.com>
2021-09-30 11:54:24 +0800

    feat: 增加StartOfDay和EndOfDay工具函数

---
+++ tools.go
@@ -0,0 +1,25 @@
+package kuu
+
+import "time"
+
+func StartOfDay(t ...time.Time) time.Time {
+	var currentTime time.Time
+	if len(t) > 0 {
+		currentTime = t[0]
+	} else {
+		currentTime = time.Now()
+	}
+	startTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location())
+	return startTime
+}
+
+func EndOfDay(t ...time.Time) time.Time {
+	var currentTime time.Time
+	if len(t) > 0 {
+		currentTime = t[0]
+	} else {
+		currentTime = time.Now()
+	}
+	endTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 23, 59, 59, 0, currentTime.Location())
+	return endTime
+}
